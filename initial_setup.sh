7512a8d7536c184305a5f76d9c388ccb0bab5748
Marcus Pereira <max171@gmail.com>
2023-05-10 22:59:01 -0300

    Fix repository username part

--- initial_setup.sh
+++ initial_setup.sh
@@ -7,7 +7,7 @@ mkdir /srv/git/ /srv/http/
 chown -R nginx:nginx /srv/ /srv/git /srv/http/
 cd /srv/git/
 sudo -u nginx bash << EOF
-git clone https://github.com/max171/ansible-lemp-auto-backup.git
+git clone https://github.com/marpere/ansible-lemp-auto-backup.git
 EOF
 ansible-galaxy install -r /srv/git/ansible-lemp-auto-backup/requirements.yml
 ansible-playbook -v /srv/git/ansible-lemp-auto-backup/configure_server.yml
